fece5c4a87df3ad8690d10614f35f1dacc875fae
Alexander <alexanderattar@gmail.com>
2019-05-24 16:47:28 -0400

    Remove transpiled files

--- packages/contracts-licensing-v2/transpiled/migrations/2_deploy_license_core.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const artifacts_1 = require("../util/artifacts");
-const { LicenseCore, LicenseCoreTest, LicenseSale, LicenseOwnership, LicenseInventory, LicenseBase, LicenseAccessControl, ERC721, SafeMath, AffiliateProgram } = new artifacts_1.Artifacts(artifacts);
-module.exports = (deployer, network) => {
-    const licenseContract = network === 'test' ? LicenseCoreTest : LicenseCore;
-    deployer.deploy(licenseContract);
-};
-//# sourceMappingURL=2_deploy_license_core.js.map
